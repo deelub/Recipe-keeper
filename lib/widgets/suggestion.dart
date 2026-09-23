21b05968c260cd50baccdb2c1236ea97e14b922c
divine <divinelubiku6@gmail.com>
2026-09-23 22:04:44 +0200

    feat : for suggested daily meals

---
+++ lib/widgets/suggestion.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class DailySuggestionsWidget extends StatelessWidget{
+  const DailySuggestionsWidget({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
