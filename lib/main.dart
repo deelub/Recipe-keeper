8f1a4bf9a3941fd26c9bc19c9827b5fce731ed34
divine <divinelubiku6@gmail.com>
2026-09-23 20:43:35 +0200

    feat : Recipe keeper navbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Recipe Keeper'),
+        ),
+        body: const Center(
+          child: Text('Recipe body'),
+        ),
+      ),
     );
   }
 }
