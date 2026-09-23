d186888f9c427fd072108b4f129a94a141e691bd
divine <divinelubiku6@gmail.com>
2026-09-23 22:20:33 +0200

    feat: Widget box for each menu item

--- lib/widgets/suggestion.dart
+++ lib/widgets/suggestion.dart
@@ -8,6 +8,28 @@ class DailySuggestionsWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      
+    );
+      }
+
+  Widget _box({required Widget child}){
+    return Container(
+      height : 120,
+      decoration: BoxDecoration(
+        color : Colors.grey,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset:  const Offset(0, 4),
+
+          ),
+        ],
+      ),
+      alignment: Alignment.center,
+      child: child,
+    );
   }
 }
