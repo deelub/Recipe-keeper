6d319ef4fb6275f280b6a4bd0d7afd01c8da651b
divine <divinelubiku6@gmail.com>
2026-09-24 00:22:49 +0200

    feat : widget for recipeoptions

--- lib/widgets/recipe_options.dart
+++ lib/widgets/recipe_options.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+
 import 'suggestion.dart';
 
-
-class RecipeOptionsWidget extends StatelessWidget{
+class RecipeOptionsWidget extends StatelessWidget {
+  const RecipeOptionsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    throw UnimplementedError();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.fromLTRB(16, 16, 16, 12),
+          child: Center(
+            child: Text(
+              "Recipes",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+        ),
+        DailySuggestionsWidget(
+          left: const Center(child: Text('Breakfast')),
+          right: const Center(child: Text('Lunch')),
+        ),
+
+        const SizedBox(height: 16),
+
+        DailySuggestionsWidget(
+          left: const Center(child: Text('Dinner')),
+          right: const Center(child: Text('Snacks')),
+        ),
+      ],
+    );
   }
 }
