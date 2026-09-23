4497ff825a5f771b6c2afcecf0eab085fbfd2dff
divine <divinelubiku6@gmail.com>
2026-09-23 23:42:57 +0200

    feat: widget body

---
+++ lib/widgets/recipe_options.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'suggestion.dart';
+
+
+class RecipeOptionsWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    
+    throw UnimplementedError();
+  }
+}
