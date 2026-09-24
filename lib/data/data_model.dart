a95db197184465acaeb942594d94796115dda02b
divine <divinelubiku6@gmail.com>
2026-09-24 02:27:05 +0200

    feat : constructor for recipe completed

---
+++ lib/data/data_model.dart
@@ -0,0 +1,24 @@
+class DataModel {
+
+  final int id;
+  final String recipeName;
+  final String prepTime;
+  final int servingSize;
+  final String ingredients;
+  final String instructions;
+
+  DataModel({
+    required this.id,
+    required this.recipeName,
+    required this.prepTime,
+    required this.servingSize,
+    required this.ingredients,
+    required this.instructions,
+
+
+
+
+
+
+  });
+}
