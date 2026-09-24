024d030659e6fb0afc4d3d8c56d8387d8bf909fd
divine <divinelubiku6@gmail.com>
2026-09-24 03:56:54 +0200

    refactor : use helper widget for options

--- lib/screens/home/breakfast_options.dart
+++ lib/screens/home/breakfast_options.dart
@@ -1,50 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_keeper/data/data_model.dart';
 import 'package:recipe_keeper/data/db_helper.dart';
+import 'package:recipe_keeper/widgets/recipe_categories.dart';
 
-
-class BreakfastOptions extends StatefulWidget{
+class BreakfastOptions extends StatefulWidget {
   const BreakfastOptions({super.key});
 
   @override
   State<BreakfastOptions> createState() => _BreakfastOptions();
 }
 
-class _BreakfastOptions extends State<BreakfastOptions>{
-
-    List<DataModel> _appRecipes = [];
+class _BreakfastOptions extends State<BreakfastOptions> {
+  List<DataModel> _appRecipes = [];
   List<DataModel> _userRecipes = [];
   bool _isLoading = true;
 
-   @override
-  void initState() {
-    super.initState();
-    _loadRecipes();
-  }
-  
-  Future<void> _loadRecipes() async {
-    final all = await DatabaseHelper.instance.getBreakfastItems();
-
-    setState(() {
-      _appRecipes = all.take(30).toList();
-      _userRecipes = all.skip(30).toList();
-      _isLoading = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CategoryRecipeList(
+      title: 'Breakfast',
+      appRecipeTile: 'Our Recipes',
+      userRecipeTile: 'Your recipes',
+      icon: Icons.free_breakfast,
+      fetcher: () => DatabaseHelper.instance.getBreakfastItems(),
+      onTapRecipe: (recipe) {},
+    );
   }
 }
-
-//  Future<List<DataModel>> DisplayUserRecipes  async {
-//    final items = await getBreakfastRecipes();
-//     return items.take(30).toList();
-// }
-
-//  Future<List<DataModel>> DisplayAppRecipes async{
-//    final items = await getBreakfastRecipes();
-//   return items.skip(30).toList();
-
-// }
