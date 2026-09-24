3e98cd79be87c2515f905beac994630931645d0c
divine <divinelubiku6@gmail.com>
2026-09-24 03:59:53 +0200

    feat : add lunch options

---
+++ lib/screens/home/lunch_options.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_keeper/data/data_model.dart';
+import 'package:recipe_keeper/data/db_helper.dart';
+import 'package:recipe_keeper/widgets/recipe_categories.dart';
+
+class BreakfastOptions extends StatefulWidget {
+  const BreakfastOptions({super.key});
+
+  @override
+  State<BreakfastOptions> createState() => _BreakfastOptions();
+}
+
+class _BreakfastOptions extends State<BreakfastOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return CategoryRecipeList(
+      title: 'Lunch',
+      appRecipeTile: 'Our Recipes',
+      userRecipeTile: 'Your recipes',
+      icon: Icons.free_breakfast,
+      fetcher: () => DatabaseHelper.instance.getBreakfastItems(),
+      onTapRecipe: (recipe) {},
+    );
+  }
+}
